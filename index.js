6bdd526d19eeaab44a7b97cec349e17168647d99
Daim <daim_choudhary@yahoo.co.uk>
2024-08-17 17:08:26 +0100

    added mobile responsiveness, finished myjourney section with added buttons

--- index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('emailbutton').addEventListener('click', function () {
-    
-    const email = 'daim_choudhary@yahoo.co.uk';
-    const subject = 'Subject Here';
-
-    window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}`;
-
-});
---
+++ script.js
@@ -0,0 +1,17 @@
+
+function openBox1() {
+    if (document.getElementById('educationPara').style.display == 'block') {
+        document.getElementById('educationPara').style.display = 'none';
+    }
+    else {
+        document.getElementById('educationPara').style.display = 'block';
+    }  
+}
+function openBox2() {
+    if (document.getElementById('workPara').style.display == 'block') {
+        document.getElementById('workPara').style.display = 'none';
+    }
+    else {
+        document.getElementById('workPara').style.display = 'block';
+    }  
+}
