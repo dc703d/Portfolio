a43b4a958548b0b0bf51f26bd07e739457e2c5f6
Daim <daim_choudhary@yahoo.co.uk>
2024-08-17 18:14:00 +0100

    added stars, thought they look quite cool, had to use some help from online sources

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ function openBox2() {
         document.getElementById('workPara').style.display = 'block';
     }  
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const starContainer = document.querySelector('.stars');
+    const starCount = 200;
+    //const size = Math.random() * 2 + 1;
+    for (let i = 0; i < starCount; i++) {
+        let star = document.createElement('div');
+        star.className = 'star';
+        star.style.top = `${Math.random() * 100}vh`;
+        star.style.left = `${Math.random() * 100}vw`;
+        star.style.animationDuration = `${Math.random() * 3 + 2}s`;
+        star.style.animationDelay = `${Math.random() * 5}s`;
+        //star.style.width = `${size}px`;
+        //star.style.height = `${size}px`;
+        starContainer.appendChild(star);
+    }
+});
