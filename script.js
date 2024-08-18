fdbeceacbb098e729c9b22fcc562ec84e13ae2f6
Daim <daim_choudhary@yahoo.co.uk>
2024-08-18 01:11:31 +0100

    added code/preview buttons, worked on stars

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
         star.style.top = `${Math.random() * 100}vh`;
         star.style.left = `${Math.random() * 100}vw`;
         star.style.animationDuration = `${Math.random() * 3 + 2}s`;
-        star.style.animationDelay = `${Math.random() * 5}s`;
+        star.style.animationDelay = `${Math.random() * 2}s`;
         //star.style.width = `${size}px`;
         //star.style.height = `${size}px`;
         starContainer.appendChild(star);
